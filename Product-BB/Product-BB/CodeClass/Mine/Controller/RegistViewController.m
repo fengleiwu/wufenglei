c5a96cbc03882649f64bc880f6e1907f390471f0
林建 <506539040@qq.com>
2016-07-20 19:36:04 +0800

    1212

--- Product-BB/Product-BB/CodeClass/Mine/Controller/RegistViewController.m
+++ Product-BB/Product-BB/CodeClass/Mine/Controller/RegistViewController.m
@@ -11,6 +11,7 @@
 @interface RegistViewController ()
 @property (nonatomic, strong)UILabel *registL;
 @property (nonatomic, strong)UILabel *explainL;
+@property (nonatomic, strong)UILabel *delegateL;
 @property (nonatomic, strong)UIButton *backB;
 @property (nonatomic, strong)UIButton *nextB;
 @property (nonatomic, strong)UIView *lineV;
@@ -43,11 +44,17 @@
     self.lineV.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.lineV];
     //创建说明标签
-    self.explainL = [[UILabel alloc]initWithFrame:CGRectMake(50, kScreenHeight*2/11+5+kScreenHeight/12, kScreenWidth-100, kScreenHeight/13)];
+    self.explainL = [[UILabel alloc]initWithFrame:CGRectMake(50, kScreenHeight*2/11+kScreenHeight/15, kScreenWidth-100, kScreenHeight/15)];
     self.explainL.textColor = [UIColor whiteColor];
     self.explainL.text = @"为了安全，我们会向你的手机发送验证码";
     self.explainL.font = [UIFont systemFontOfSize:13];
     [self.view addSubview:self.explainL];
+    //创建协议标签
+    self.delegateL = [[UILabel alloc]initWithFrame:CGRectMake(50, kScreenHeight*3/11 +kScreenHeight/15+kScreenHeight/12, kScreenWidth/2, kScreenHeight/15)];
+    self.delegateL.textColor = [UIColor whiteColor];
+    self.delegateL.text = @"本人已阅读并同意";
+    self.delegateL.font = [UIFont systemFontOfSize:13];
+    [self.view addSubview:self.delegateL];
     //创建返回按钮
     self.backB = [UIButton buttonWithType:UIButtonTypeSystem];
     self.backB.frame = CGRectMake(10, 50, 20,20);
@@ -56,17 +63,24 @@
     [self.backB addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.backB];
     //创建手机号码验证框
-    self.phoneTF = [[UITextField alloc]initWithFrame:CGRectMake(50, kScreenHeight*2/11, kScreenWidth-100, kScreenHeight/12)];
+    self.phoneTF = [[UITextField alloc]initWithFrame:CGRectMake(50, kScreenHeight*2/11, kScreenWidth-100, kScreenHeight/15)];
     self.phoneTF.layer.masksToBounds = YES;
     self.phoneTF.layer.cornerRadius = 6;
+    UILabel *label1 = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 35, 30)];
+    label1.text = @"+86";
+    label1.textColor = [UIColor grayColor];
+    self.phoneTF.leftView = label1;
+    self.phoneTF.leftViewMode = UITextFieldViewModeAlways;
     self.phoneTF.textColor = [UIColor whiteColor];
     UIColor *color = [UIColor whiteColor];
     self.phoneTF.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"请输入手机号" attributes:@{NSForegroundColorAttributeName:color}];
+    self.phoneTF.layer.borderColor= [UIColor whiteColor].CGColor;
+    self.phoneTF.layer.borderWidth= 1.0f;
     [self.phoneTF addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
     [self.view addSubview:self.phoneTF];
     //创建下一步按钮
     self.nextB = [UIButton buttonWithType:UIButtonTypeSystem];
-    self.nextB.frame = CGRectMake(50, kScreenHeight*3/11 + 20, kScreenWidth-100, kScreenHeight/12);
+    self.nextB.frame = CGRectMake(50, kScreenHeight*3/11 +kScreenHeight/15, kScreenWidth-100, kScreenHeight/12);
     [self.nextB setTitle:@"下一步" forState:UIControlStateNormal];
     [self.nextB setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [self.nextB.layer setMasksToBounds:YES];
